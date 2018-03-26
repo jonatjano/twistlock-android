3d9e05924d1f1928c91d1081354b7a360aa2dccb
jonatjano <jonathan.selle@laposte.net>
2018-03-26 23:11:29 +0200

    Initial commit

---
+++ app/src/main/java/fr/javane/twistlock/MainActivity.java
@@ -0,0 +1,15 @@
+package fr.javane.twistlock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
